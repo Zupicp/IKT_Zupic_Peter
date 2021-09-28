e6ae677d58846d3f7221ce1e748366a81dd0cb48
Zupicp <71979877+Zupicp@users.noreply.github.com>
2021-09-28 12:28:55 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <form action="/action_page.php">
             <label for="uname">Username</label><br>
-            <input type="text" id="uname" name="uname" placeholder="Username">><br>
+            <input type="text" id="uname" name="uname" placeholder="Username"><br>
             <label for="pword">Password</label><br>
-            <input type="text" id="pword" name="pword" placeholder="Password">><br><br>
+            <input type="text" id="pword" name="pword" placeholder="Password"><br><br>
             <input type="submit" value="Login">
           </form> <br>
-          <a href="" target="_blank">Regisztáció</a>
+          <a href="ikt.php?id=reglap" target="_blank">Regisztáció</a>
