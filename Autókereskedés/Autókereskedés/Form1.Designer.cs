2d53fb456e87aabde37efbc7c77d82ff48cf1e5b
Zupicp <71979877+Zupicp@users.noreply.github.com>
2021-11-02 12:22:58 +0100

    Autokereskedes

---
+++ Autókereskedés/Autókereskedés/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Autókereskedés
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Deleteb = new System.Windows.Forms.Button();
+            this.Addb = new System.Windows.Forms.Button();
+            this.Changeb = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMűveletekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.névjegyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.megnyitásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mentésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.újLétrehozásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.névjegyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Deleteb
+            // 
+            this.Deleteb.Location = new System.Drawing.Point(65, 246);
+            this.Deleteb.Name = "Deleteb";
+            this.Deleteb.Size = new System.Drawing.Size(136, 23);
+            this.Deleteb.TabIndex = 0;
+            this.Deleteb.Text = "Törlés";
+            this.Deleteb.UseVisualStyleBackColor = true;
+            this.Deleteb.Click += new System.EventHandler(this.Deleteb_Click);
+            // 
+            // Addb
+            // 
+            this.Addb.Location = new System.Drawing.Point(405, 245);
+            this.Addb.Name = "Addb";
+            this.Addb.Size = new System.Drawing.Size(130, 24);
+            this.Addb.TabIndex = 1;
+            this.Addb.Text = "Hozzáad";
+            this.Addb.UseVisualStyleBackColor = true;
+            this.Addb.Click += new System.EventHandler(this.Addb_Click);
+            // 
+            // Changeb
+            // 
+            this.Changeb.Location = new System.Drawing.Point(207, 246);
+            this.Changeb.Name = "Changeb";
+            this.Changeb.Size = new System.Drawing.Size(131, 23);
+            this.Changeb.TabIndex = 2;
+            this.Changeb.Text = "Módosít";
+            this.Changeb.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMűveletekToolStripMenuItem,
+            this.névjegyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMűveletekToolStripMenuItem
+            // 
+            this.fileMűveletekToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.megnyitásToolStripMenuItem,
+            this.mentésToolStripMenuItem,
+            this.újLétrehozásToolStripMenuItem});
+            this.fileMűveletekToolStripMenuItem.Name = "fileMűveletekToolStripMenuItem";
+            this.fileMűveletekToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.fileMűveletekToolStripMenuItem.Text = "File műveletek";
+            // 
+            // névjegyToolStripMenuItem
+            // 
+            this.névjegyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.névjegyToolStripMenuItem1,
+            this.kilépésToolStripMenuItem});
+            this.névjegyToolStripMenuItem.Name = "névjegyToolStripMenuItem";
+            this.névjegyToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.névjegyToolStripMenuItem.Text = "Névjegy";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Adatok";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(405, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(405, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(405, 168);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(405, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Szín";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(541, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Rendszám";
+           
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(541, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Gyártás éve";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(541, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Típus";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(541, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Márka";
+            // 
+            // megnyitásToolStripMenuItem
+            // 
+            this.megnyitásToolStripMenuItem.Name = "megnyitásToolStripMenuItem";
+            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.megnyitásToolStripMenuItem.Text = "Megnyitás";
+            this.megnyitásToolStripMenuItem.Click += new System.EventHandler(this.megnyitásToolStripMenuItem_Click);
+            // 
+            // mentésToolStripMenuItem
+            // 
+            this.mentésToolStripMenuItem.Name = "mentésToolStripMenuItem";
+            this.mentésToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.mentésToolStripMenuItem.Text = "Mentés";
+            this.mentésToolStripMenuItem.Click += new System.EventHandler(this.mentésToolStripMenuItem_Click);
+            // 
+            // újLétrehozásToolStripMenuItem
+            // 
+            this.újLétrehozásToolStripMenuItem.Name = "újLétrehozásToolStripMenuItem";
+            this.újLétrehozásToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.újLétrehozásToolStripMenuItem.Text = "Új létrehozás";
+            // 
+            // kilépésToolStripMenuItem
+            // 
+            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.kilépésToolStripMenuItem_Click);
+            // 
+            // névjegyToolStripMenuItem1
+            // 
+            this.névjegyToolStripMenuItem1.Name = "névjegyToolStripMenuItem1";
+            this.névjegyToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.névjegyToolStripMenuItem1.Text = "Névjegy";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(65, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(273, 212);
+            this.listBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Changeb);
+            this.Controls.Add(this.Addb);
+            this.Controls.Add(this.Deleteb);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Deleteb;
+        private System.Windows.Forms.Button Addb;
+        private System.Windows.Forms.Button Changeb;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMűveletekToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem névjegyToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripMenuItem megnyitásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mentésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem újLétrehozásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem névjegyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Autókereskedés/Autókereskedés/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Autókereskedés
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void megnyitásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog nyit = new OpenFileDialog();
+            nyit.Filter = "*.txt | *.txt";
+            nyit.ShowDialog();
+            filename = nyit.FileName;
+            foreach (var item in File.ReadAllLines(filename))
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+        string filename = "";
+        private void mentésToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (filename!="")
+                {
+                    StreamWriter sw = new StreamWriter(filename);
+                    SaveFileDialog ment = new SaveFileDialog();
+                    foreach(var item in listBox1.Items)
+                    {
+                        sw.WriteLine(item);
+                    }
+                    sw.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Nem!");
+            }
+            
+        }
+
+        private void Addb_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string marka = textBox1.Text;
+                string tipus = textBox2.Text;
+                int ev = Convert.ToInt32(textBox3.Text);
+                string szin = textBox4.Text;
+                string rendszam = textBox5.Text;
+                listBox1.Items.Add(marka + ";" + tipus + ";" + ev + ";" + szin + ";" + rendszam);
+                
+            }
+            catch (Exception)
+            {
+               
+                MessageBox.Show("Valami üres vagy rossz formátum!");
+            }
+                  }
+
+        private void Deleteb_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void kilépésToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
